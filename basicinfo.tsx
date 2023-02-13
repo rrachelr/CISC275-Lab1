291d2c00c4339a95b20db313d56840037dac53a4
rrachelr <rachrobins1@gmail.com>
2023-02-13 10:19:51 -0500

    edited info

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -2,10 +2,10 @@
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
-const yourName : String = "Rachel Robins";
-let yourPronouns : String = "She/Her";
-const yourMajor : String = "Computer Science";
+const yourName : String = "Rachel";
+let yourPronouns : String = "she/her";
+const yourMajor : String = "computer science";
 let classification : String = "sophomore";
-let oneExcite : String = "Making a cool web app";
-let anyConcerns : String = "Getting stuck with a bad group";
-let funFact : String = "I have two cats";
+let oneExcite : String = "making a cool web app";
+let anyConcerns : String = "getting stuck with a bad group";
+let funFact : String = "i have two cats";
